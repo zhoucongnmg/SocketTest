6fe0e511ec234cfa405d28b9188ccd131c526520
zhoucong <zhoucong@zhoucong-PC>
2016-05-27 17:28:42 +0800

    5.27

--- src/Tcp/Client.java
+++ src/Tcp/Client.java
@@ -17,8 +17,8 @@ public class Client {
 		try {
 			//1.创建客户端Socket，指定服务器地址和端口
 			Socket socket=new Socket("localhost", 8888);
+
 			//2.获取输出流，向服务器端发送信息
-			
 			OutputStream os=socket.getOutputStream();//字节输出流
 			PrintWriter pw=new PrintWriter(os);//将输出流包装为打印流
 			pw.write("用户名：alice;密码：789");
--- src/Tcp/Server.java
+++ src/Tcp/Server.java
@@ -18,15 +18,19 @@ public class Server {
 	public static void main(String[] args) {
 		try {
 			//1.创建一个服务器端Socket，即ServerSocket，指定绑定的端口，并监听此端口
-			ServerSocket serverSocket=new ServerSocket(8888);
+			//backlog输入连接指示（对连接的请求）的最大队列长度被设置为 backlog 参数。如果队列满时收到连接指示，则拒绝该连接。
+			int backlog = 2;			
+			ServerSocket serverSocket=new ServerSocket(8888,backlog);
+            //serverSocket.bind(8888);
 			Socket socket=null;
 			//记录客户端的数量
 			int count=0;
 			System.out.println("***服务器即将启动，等待客户端的连接***");
+			
 			//循环监听等待客户端的连接
 			while(true){
 				//调用accept()方法开始监听，等待客户端的连接
-				socket=serverSocket.accept();
+				socket=serverSocket.accept();   //从连接请求队列中取出连接,所以此时请求队列一直为空，backlog失效
 				//创建一个新的线程
 				ServerThread serverThread=new ServerThread(socket);
 				//启动线程
@@ -36,7 +40,9 @@ public class Server {
 				System.out.println("客户端的数量："+count);	
 				InetAddress address=socket.getInetAddress();
 				System.out.println("当前客户端的IP："+address.getHostAddress());
+				//break;
 			}
+
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
